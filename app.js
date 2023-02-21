2bea2bfaaae4b089ccc7ab35d6ac2a8fdc4d3a6e
Nam Nguyen <nam.nguyen201@gmail.com>
2023-02-21 11:57:52 -0800

    Create shopping list that bolds items when hover over

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+const GroceryListItem = (props) => {
+
+  const [isDone, setIsDone] = React.useState(false);
+
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal',
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setIsDone(!isDone)}>
+      {props.toBuy}
+    </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem toBuy={item} />
+    ))}
+  </ul>
+);
+
+const Cucumbers = () => (
+  <li>
+    Cucumbers
+  </li>
+)
+
+const Kale = () => (
+  <li>
+    Kale
+  </li>
+)
+const App = () => (
+    <div>
+      <h1>Shopping List</h1>
+      <GroceryList items={[<Cucumbers />, <Kale />]}/>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
